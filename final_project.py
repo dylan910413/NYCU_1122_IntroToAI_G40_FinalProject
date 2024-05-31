b6a33c57ad6d50f8bd291f561ac93996701254de
dylan910413 <dylan910413@gmail.com>
2024-05-31 16:20:26 +0000

    6/1

--- final_project.py
+++ final_project.py
@@ -1,186 +1,137 @@
-# You are not allowed to import any additional packages/libraries.
 import numpy as np
 from pandas import DataFrame, read_csv
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
-# This function computes the gini impurity of a label array.
-def gini(y):
-    unique_classes, counts = np.unique(y, return_counts=True)
-    total_samples = len(y)
-    gini_impurity = 1
-    for count in counts:
-        proportion = count / total_samples
-        gini_impurity -= proportion ** 2
-    return gini_impurity
+def gini(y, weight):
+    classes = np.unique(y)
+    gini = 1
+    if weight is None:
+        for cls in classes:
+            proportion = len(y[np.where(y == cls)]) / len(y)
+            gini -= proportion**2
+    else:
+        total_weight = np.sum(np.array(weight))
+        for cls in classes:
+            proportion = np.sum(weight[np.where(y == cls)]) / total_weight
+            gini -= proportion**2
+    return gini
 
-# This function computes the entropy of a label array.
-def entropy(y):
-    unique_classes, counts = np.unique(y, return_counts=True)
-    total_samples = len(y)
-    entropy_val = 0
-    for count in counts:
-        proportion = count / total_samples
-        entropy_val -= proportion * np.log2(proportion)
-    return entropy_val
+class Node():
+    def __init__(self, feature=None, threshold=None, left=None, right=None, cls=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.cls = cls
         
-# The decision tree classifier class.
-# Tips: You may need another node class and build the decision tree recursively.
-class Node:
-    def __init__(self, feature_index=None, split_value=None, left=None, right=None, value=None):
-        self.feature_index = feature_index  # Feature index for splitting
-        self.split_value = split_value      # Value used for splitting
-        self.left = left                    # Left child node
-        self.right = right                  # Right child node
-        self.value = value                  # Value if the node is a leaf node (class prediction)
-    def is_leaf_node(self):
-        return self.value is not None
-    
 class DecisionTree():
     def __init__(self, criterion='gini', max_depth=None):
         self.criterion = criterion
         self.max_depth = max_depth 
-        self.tree = None
     
-    # This function computes the impurity based on the criterion.
-    def impurity(self, y):
-        if self.criterion == 'gini':
-            return gini(y)
-        elif self.criterion == 'entropy':
-            return entropy(y)
+    def impurity(self, y, weight=None):
+        return gini(y, weight)
     
-    # This function fits the given data using the decision tree algorithm.
-    def fit(self, X, y):
-        self.tree = self._build_tree(X, y, depth=0)
+    def fit(self, X, y, weight=None):
+        y = y.reshape(-1, 1)
+        self.feature_importance = np.zeros(X.shape[1])
+        data = np.concatenate((X, y), axis=1)
+        self.root = self.build_tree(data, 0, weight)
     
-    # This function takes the input data X and predicts the class label y according to your trained model.
+    def build_tree(self, data, depth, weight):
+        X, y = data[:, :-1], data[:, -1]
+        num_features = X.shape[1]
+        if depth < self.max_depth:
+            split = self.get_split(data, num_features, weight)
+            if split['information_gain'] > 0:
+                left_weight, right_weight = split['left_weight'], split['right_weight']
+                left_subtree = self.build_tree(split['left_data'], depth + 1, left_weight)
+                right_subtree = self.build_tree(split['right_data'], depth + 1, right_weight)
+                self.feature_importance[split['feature']] += 1
+                return Node(split['feature'], split['threshold'], left_subtree, right_subtree)
+
+        y = list(y)
+        cls = max(y, key=y.count)
+        return Node(cls = cls)
+    
+    def get_split(self, data, num_features, weight):
+        best_gain = {'feature': None, 'threshold': None, 'information_gain': 0,
+                'left_data': None, 'right_data': None, 'left_weight': None, 'right_weight': None}
+        max_gain = 0
+        features = np.arange(num_features)
+        for feature in features:
+            col = data[:, feature]
+            thresholds = np.unique(col)
+            for threshold in thresholds:
+                left_data , right_data = data[np.where(col <= threshold)], data[np.where(col > threshold)]
+                if len(left_data) and len(right_data):
+                    y_total, y_left, y_right = data[:, -1], left_data[:, -1], right_data[:, -1]
+                    
+                    if weight is None:
+                        left_weight, right_weight = None, None
+                        left_ratio, right_ratio = len(y_left) / len(y_total), len(y_right) / len(y_total)
+                    else:
+                        left_weight, right_weight = weight[np.where(col <= threshold)], weight[np.where(col > threshold)]
+                        left_ratio, right_ratio = np.sum(left_weight) / np.sum(weight), np.sum(right_weight) / np.sum(weight)
+                        
+                    information_gain = self.impurity(y_total, weight) - (left_ratio * self.impurity(y_left, left_weight) + right_ratio * self.impurity(y_right, right_weight))
+                    if information_gain > max_gain:
+                        max_gain = information_gain
+                        best_gain = {'feature': feature, 'threshold': threshold, 'information_gain': information_gain,
+                                'left_data': left_data, 'right_data': right_data,
+                                'left_weight': left_weight, 'right_weight': right_weight}
+        return best_gain
+    
     def predict(self, X):
-        return [self._traverse_tree(x, self.tree) for x in X]
+        pred_y = []
+        for x in X:
+            pred_y.append(self.make_predict(x, self.root))
+        return np.array(pred_y)
     
-    # This function plots the feature importance of the decision tree.
-    def plot_feature_importance_img(self, columns):
-        feature_usage = np.zeros(len(columns))
-        self._count_feature_usage(self.tree, feature_usage)
-        
-        plt.figure(figsize=(10, 6))
-        plt.barh(columns, feature_usage)
-        plt.xlabel('Feature Usage')
-        plt.title('Decision Tree Feature Importance')
-        plt.show()
+    def make_predict(self, x, node):
+        if node.cls != None:
+            return node.cls
+        return self.make_predict(x, node.left) if x[node.feature] <= node.threshold else self.make_predict(x, node.right)
 
-    def _count_feature_usage(self, node, feature_usage):
-        if node is None:
-            return
-        
-        if node.feature_index is not None:
-            feature_usage[node.feature_index] += 1
-        
-        self._count_feature_usage(node.left, feature_usage)
-        self._count_feature_usage(node.right, feature_usage)
-
-    def _build_tree(self, X, y, depth):
-        n_samples, n_features = X.shape
-        n_classes = len(np.unique(y))
-
-        if depth >= self.max_depth or n_classes == 1 or n_samples < 2:
-            leaf_value = np.argmax(np.bincount(y))
-            return Node(value=leaf_value)
-
-        best_feature, best_threshold = None, None
-        best_impurity = float('inf')
-
-        for feature_idx in range(n_features):
-            unique_values = np.unique(X[:, feature_idx])
-            for threshold in unique_values:
-                left_indices = np.where(X[:, feature_idx] <= threshold)[0]
-                right_indices = np.where(X[:, feature_idx] > threshold)[0]
-                if len(left_indices) > 0 and len(right_indices) > 0:
-                    left_impurity = self.impurity(y[left_indices])
-                    right_impurity = self.impurity(y[right_indices])
-
-                    total_impurity = (len(left_indices) * left_impurity + len(right_indices) * right_impurity) / n_samples
-
-                    if total_impurity < best_impurity:
-                        best_impurity = total_impurity
-                        best_feature = feature_idx
-                        best_threshold = threshold
-
-        if best_feature is not None:
-            left_idxs = np.where(X[:, best_feature] <= best_threshold)[0]
-            right_idxs = np.where(X[:, best_feature] > best_threshold)[0]
-            left_child = self._build_tree(X[left_idxs], y[left_idxs], depth + 1)
-            right_child = self._build_tree(X[right_idxs], y[right_idxs], depth + 1)
-            
-            if left_child is None:
-                left_child = Node(value=np.argmax(np.bincount(y[left_idxs])))
-            if right_child is None:
-                right_child = Node(value=np.argmax(np.bincount(y[right_idxs])))
-            return Node(feature_index=best_feature, split_value=best_threshold, left=left_child, right=right_child)
-
-        leaf_value = np.argmax(np.bincount(y))
-        return Node(value=leaf_value)
-    def _traverse_tree(self, sample, node):
-        if node is None:  
-            return None
-        if node.is_leaf_node():
-            return node.value
-        if sample[node.feature_index] <= node.split_value:
-            return self._traverse_tree(sample, node.left)
-        else:
-            return self._traverse_tree(sample, node.right)
-        
-        
-# The AdaBoost classifier class.
-# best = 250
 class AdaBoost():
-    def __init__(self, criterion='gini', n_estimators = 0):
+    def __init__(self, criterion='gini', n_estimators=200):
         self.criterion = criterion 
         self.n_estimators = n_estimators
-        self.models = []  
-        self.model_weights = []
-        self.sample_weights = []
-    # This function fits the given data using the AdaBoost algorithm.
-    # You need to create a decision tree classifier with max_depth = 1 in each iteration.
+        self.clfs = []
+        self.alpha = []
+
     def fit(self, X, y):
         n_samples = X.shape[0]
-        self.sample_weights = np.ones(n_samples) / n_samples 
+        weight = np.full(n_samples, 1 / n_samples)
         for _ in range(self.n_estimators):
-            sampled_indices = np.random.choice(n_samples, n_samples, replace=True, p=self.sample_weights)
-            X_sampled = X[sampled_indices]
-            y_sampled = y[sampled_indices]
-            
-            tree = DecisionTree(criterion=self.criterion, max_depth=1)
-            tree.fit(X_sampled, y_sampled) 
-            
-            y_pred_sampled = np.array(tree.predict(X_sampled))
-            incorrect = (y_pred_sampled != y)
-            err = np.sum(self.sample_weights * (incorrect)) 
-            alpha = 0.5 * np.log((1 - err) / max(err, 1e-10))
-            
-            self.sample_weights *= np.exp(alpha * incorrect)
-            self.sample_weights /= np.sum(self.sample_weights)
-            
-            self.models.append(tree)
-            self.model_weights.append(alpha)
+            clf = DecisionTree(max_depth = 1, criterion=self.criterion)
+            clf.fit(X, y, weight)
+            y_pred = clf.predict(X)
+            self.clfs.append(clf)
+
+            error_samples = (y != y_pred)
+            error = np.sum(error_samples * weight)
+#test 0.3 ~ 0.7 (0.3 = best)
+            alpha = 0.3 * np.log((1 - error) / error)
+            self.alpha.append(alpha)
+            weight *= np.exp([alpha if error else - alpha for error in error_samples])
+            weight /= np.sum(weight)
 
     def predict(self, X):
-        n_samples = len(X)
-        total_alpha = 0
-        final_predictions = np.zeros(n_samples)
-        
-        for i, model in enumerate(self.models):
-            total_alpha += self.model_weights[i]
-            predictions = np.array(model.predict(X))
-            final_predictions += self.model_weights[i] * predictions
+        y_pred = []
+        for x in X:
+            total = 0
+            for i, clf in enumerate(self.clfs):
+                prediction = clf.make_predict(x, clf.root)
+                if prediction == 0:
+                    total -= self.alpha[i]
+                else:
+                    total += self.alpha[i]
+            y_pred.append(0 if total < 0 else 1)
+        return np.array(y_pred)
 
-        final_predictions /= total_alpha 
-        final_predictions = np.where(final_predictions > 0, 1, 0)
-
-        return final_predictions
-# Do not modify the main function architecture.
-# You can only modify the value of the random seed and the the arguments of your Adaboost class.
 if __name__ == "__main__":
-# Data Loading
     train_df = DataFrame(read_csv("train.csv"))
     test_df = DataFrame(read_csv("test.csv"))
     X_train = train_df.drop(["target"], axis=1)
@@ -193,31 +144,20 @@ if __name__ == "__main__":
     X_test = X_test.to_numpy()
     y_test = y_test.to_numpy()
 
-# Set random seed to make sure you get the same result every time.
-# You can change the random seed if you want to.
     np.random.seed(0)
-# Decision Tree
-    print("Part 1: Decision Tree")
-    data = np.array([0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1])
-    print(f"gini of [0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]: {gini(data)}")
-    print(f"entropy of [0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]: {entropy(data)}")
+    print("Decision Tree")
     tree = DecisionTree(criterion='gini', max_depth=7)
     tree.fit(X_train, y_train)
     y_pred = tree.predict(X_test)
     print("Accuracy (gini with max_depth=7):", accuracy_score(y_test, y_pred))
-    tree = DecisionTree(criterion='entropy', max_depth=7)
-    tree.fit(X_train, y_train)
-    y_pred = tree.predict(X_test)
-    print("Accuracy (entropy with max_depth=7):", accuracy_score(y_test, y_pred))
-    #columns = ["age","sex","cp","fbs","thalach","thal"]
-    #tree.plot_feature_importance_img(columns)
+    
+# Plotting the feature importance
+    # columns = train_df.iloc[:, :-1].columns
+    # tree.plot_feature_importance_img(columns)
+
 # AdaBoost
-    print("Part 2: AdaBoost")
-    # Tune the arguments of AdaBoost to achieve higher accuracy than your Decision Tree.
-    ada = AdaBoost(criterion='gini', n_estimators=250)
+    print("AdaBoost")
+    ada = AdaBoost(criterion='gini', n_estimators=7)
     ada.fit(X_train, y_train)
     y_pred = ada.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, y_pred))
-    
-
-
